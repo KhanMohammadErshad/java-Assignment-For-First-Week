d06a8548793d90b046db09a6c2544d9c967016c1
KhanMohammadErshad <106887926+KhanMohammadErshad@users.noreply.github.com>
2022-10-25 20:54:21 +0430

    Update OOP.java

--- OOP.java
+++ OOP.java
@@ -13,10 +13,10 @@ public class OOP
         BigInteger intNumber = BigInteger.valueOf(123434344); //BigInteger can represent an integer of any size.
         BigDecimal realNumber = BigDecimal.valueOf(23452345.23452345);// BigDecimal can represent a real number of any size.
 
-        Object nameOfAuthor = "Hamdullah Hamdard";
+        Object nameOfAuthor = "khan Mohammad";
 
         abstraction obj = new abstraction();
-        obj.add(1,"Hamdullah");
+        obj.add(1,"khan");
         System.out.println("Name of Author: "+nameOfAuthor);
         System.out.println("SID: "+obj.getId()+" STnAME: "+obj.getName()+" SubName: "+obj.getSubName()+" faculty Name"+obj.getFacultyName());
 
