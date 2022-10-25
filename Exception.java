81fe23aa1fd5beff13cc5e5ea423e5a056f33859
KhanMohammadErshad <106887926+KhanMohammadErshad@users.noreply.github.com>
2022-10-25 20:48:36 +0430

    Update Exception.java

--- Exception.java
+++ Exception.java
@@ -4,6 +4,6 @@ public class Exception
 {
     public static void main(String[] args)
     {
-        System.out.println("My Name is Hamdullah hamdard ");
+        System.out.println("khan ");
     }
 }
